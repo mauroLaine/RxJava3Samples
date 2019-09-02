6f46d85c8188d3d26e42288bbe76efee114b4fbb
Mauricio Laine <laine.mau@gmail.com>
2019-09-02 12:58:20 -0700

    Added a try and from method to create different observables

--- app/src/main/java/com/laine/mauro/rxjava3samples/samples/RxJava3Sample.java
+++ app/src/main/java/com/laine/mauro/rxjava3samples/samples/RxJava3Sample.java
@@ -1,5 +1,9 @@
 package com.laine.mauro.rxjava3samples.samples;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import io.reactivex.Observable;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
@@ -10,8 +14,6 @@ public class RxJava3Sample {
 
     public static void main(String[] args) {
 
-        final Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
-
         final Consumer<Integer> consumer = new Consumer<Integer>() {
             @Override
             public void accept(Integer integer) throws Throwable {
@@ -20,12 +22,29 @@ public class RxJava3Sample {
         };
 
         final CompositeDisposable compositeDisposable = new CompositeDisposable();
-        final Disposable disposable = observable.map(new Function<Integer, Integer>() {
+        final Disposable disposable = tryFrom(consumer);
+        compositeDisposable.add(disposable);
+    }
+
+
+    private static Disposable tryJust(Consumer<Integer> consumer) {
+        final Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
+        return observable.map(new Function<Integer, Integer>() {
             @Override
             public Integer apply(Integer integer) throws Throwable {
                 return integer + 10;
             }
         }).subscribe(consumer);
-        compositeDisposable.add(disposable);
+    }
+
+    private static Disposable tryFrom(Consumer<Integer> consumer) {
+        final Integer[] numbers = {10, 20, 30, 40, 50};
+        final Observable<Integer> observable = Observable.fromArray(numbers);
+        return observable.map(new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer) throws Throwable {
+                return integer * 10;
+            }
+        }).subscribe(consumer);
     }
 }
